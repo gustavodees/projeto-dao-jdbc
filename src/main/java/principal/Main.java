618e448d539e1f322ca27c35384a681fd2b9f63f
ChocolateKest <gustavoemartins@gmail.com>
2025-09-18 20:16:06 -0300

    [200~findByDepartment implementation

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -7,6 +7,7 @@ import model.dao.SellerDao;
 import model.dao.impl.SellerDaoJDBC;
 
 import java.util.Date;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,9 +18,13 @@ public class Main {
       SellerDao sellerDao = DaoFactory.createSellerDao();
       System.out.println("======= Test 1: seller findById ========");
       Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
-
+        System.out.println("======= Test 2: seller findById ========");
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller s : list){
+            System.out.println(s);
+        }
 
     }
 }
